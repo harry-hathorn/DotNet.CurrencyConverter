2bd632f911aeded0317b8a845a5a815c0487def8
Harry Hathorn <hbhhathorn@gmail.com>
2025-02-05 15:20:35 +0400

    FrankfurterExchangeProvider test, calls api

---
+++ tests/UnitTests/Infrastructure/ExchangeProviders/FrankfurterExchangeProvider.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace UnitTests.Infrastructure.ExchangeProviders
+{
+    internal class FrankfurterExchangeProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public FrankfurterExchangeProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        internal async Task GetLatestExchangeRatesAsync()
+        {
+            await _httpClient.GetAsync("https://api.frankfurter.dev/v1/latest");
+        }
+    }
+}
---
+++ tests/UnitTests/Infrastructure/ExchangeProviders/GetLatestExchangeRatesShould.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Moq.Protected;
+
+namespace UnitTests.Infrastructure.ExchangeProviders
+{
+    public class GetLatestExchangeRatesShould
+    {
+        private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+        private readonly FrankfurterExchangeProvider _exchangeProvider;
+
+        public GetLatestExchangeRatesShould()
+        {
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            _mockHttpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            ).ReturnsAsync(new HttpResponseMessage());
+            var httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+            _exchangeProvider = new FrankfurterExchangeProvider(httpClient);
+        }
+        [Fact]
+        public async Task CallApiOnce()
+        {
+            await _exchangeProvider.GetLatestExchangeRatesAsync();
+            var expectedUri = new Uri("https://api.frankfurter.dev/v1/latest");
+            _mockHttpMessageHandler.Protected().Verify<Task<HttpResponseMessage>>("SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Get
+                    && req.RequestUri == expectedUri // to this uri
+                ),
+                ItExpr.IsAny<CancellationToken>());
+        }
+    }
+}
