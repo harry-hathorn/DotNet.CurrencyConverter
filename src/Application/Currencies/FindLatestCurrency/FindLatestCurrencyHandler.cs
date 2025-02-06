caf7edabdb2c2441dbc5227933f86ceeeaa8156c
Harry Hathorn <hbhhathorn@gmail.com>
2025-02-06 16:59:29 +0400

    should fail when no provider found

--- src/Application/Currencies/FindLatestCurrency/FindLatestCurrencyHandler.cs
+++ src/Application/Currencies/FindLatestCurrency/FindLatestCurrencyHandler.cs
@@ -1,21 +1,30 @@
 ﻿using Domain.Common;
 using Domain.Currencies;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Currencies.FindLatestCurrency
 {
     public class FindLatestCurrencyHandler : IRequestHandler<FindLatestCurrencyQuery, Result>
     {
-        private IExchangeProviderFactory _exchangeFactory;
+        private readonly IExchangeProviderFactory _exchangeFactory;
+        private readonly ILogger<FindLatestCurrencyHandler> _logger;
 
-        public FindLatestCurrencyHandler(IExchangeProviderFactory exchangeFactory)
+        public FindLatestCurrencyHandler(IExchangeProviderFactory exchangeFactory,
+            ILogger<FindLatestCurrencyHandler> logger)
         {
             _exchangeFactory = exchangeFactory;
+            _logger = logger;
         }
 
         public async Task<Result> Handle(FindLatestCurrencyQuery command, CancellationToken cancellationToken)
         {
             var exchangeProvider = _exchangeFactory.GetProvider(ExchangeProviderType.Frankfurter);
+            if (exchangeProvider == null)
+            {
+                _logger.LogError("Could not find an exchange provider, {requestedProvider}", ExchangeProviderType.Frankfurter);
+                return Result.Failure(Error.SystemError);
+            }
             return null;
         }
     }
--- tests/UnitTests/Application/FindLatestCurrencyShould.cs
+++ tests/UnitTests/Application/FindLatestCurrencyShould.cs
@@ -1,6 +1,11 @@
 ﻿using Application.Currencies.FindLatestCurrency;
+using Castle.Core.Logging;
+using Domain.Common;
 using Domain.Currencies;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Moq;
+using System.Xml.XPath;
 
 namespace UnitTests.Application
 {
@@ -12,7 +17,8 @@ namespace UnitTests.Application
         public FindLatestCurrencyShould()
         {
             _factory = new Mock<IExchangeProviderFactory>();
-            _handler = new FindLatestCurrencyHandler(_factory.Object);
+            _handler = new FindLatestCurrencyHandler(_factory.Object, 
+                new Mock<ILogger<FindLatestCurrencyHandler>>().Object);
         }
 
         [Fact]
@@ -20,6 +26,14 @@ namespace UnitTests.Application
         {
             await _handler.Handle(new FindLatestCurrencyQuery("GBP"), default);
             _factory.Verify(x => x.GetProvider(ExchangeProviderType.Frankfurter), Times.Once);
+        }
+
+        [Fact]
+        public async Task ReturnSystemErrorIfNoProvider()
+        {
+            var result = await _handler.Handle(new FindLatestCurrencyQuery("GBP"), default);
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(Error.SystemError);
         }
     }
 }
