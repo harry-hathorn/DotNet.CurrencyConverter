9ef2912c0bd2308056618aea114b914078a9422b
Harry Hathorn <hbhhathorn@gmail.com>
2025-02-06 17:04:01 +0400

    Find latest fails for invalid currency

--- src/Application/Currencies/FindLatestCurrency/FindLatestCurrencyHandler.cs
+++ src/Application/Currencies/FindLatestCurrency/FindLatestCurrencyHandler.cs
@@ -19,12 +19,18 @@ namespace Application.Currencies.FindLatestCurrency
 
         public async Task<Result> Handle(FindLatestCurrencyQuery command, CancellationToken cancellationToken)
         {
+            var currencyCodeResult = CurrencyCode.FromCode(command.currencyCode);
+            if (currencyCodeResult.IsFailure)
+            {
+                return currencyCodeResult;
+            }
             var exchangeProvider = _exchangeFactory.GetProvider(ExchangeProviderType.Frankfurter);
             if (exchangeProvider == null)
             {
                 _logger.LogError("Could not find an exchange provider, {requestedProvider}", ExchangeProviderType.Frankfurter);
                 return Result.Failure(Error.SystemError);
             }
+
             return null;
         }
     }
--- tests/UnitTests/Application/FindLatestCurrencyShould.cs
+++ tests/UnitTests/Application/FindLatestCurrencyShould.cs
@@ -11,13 +11,18 @@ namespace UnitTests.Application
 {
     public class FindLatestCurrencyShould
     {
-        private readonly Mock<IExchangeProviderFactory> _factory;
+        private readonly Mock<IExchangeProvider> _exchangeProviderMock;
+        private readonly Mock<IExchangeProviderFactory> _factoryMock;
         private readonly FindLatestCurrencyHandler _handler;
 
         public FindLatestCurrencyShould()
         {
-            _factory = new Mock<IExchangeProviderFactory>();
-            _handler = new FindLatestCurrencyHandler(_factory.Object, 
+            _factoryMock = new Mock<IExchangeProviderFactory>();
+            _exchangeProviderMock = new Mock<IExchangeProvider>();
+            _factoryMock.Setup(x => x.GetProvider(It.IsAny<ExchangeProviderType>()))
+                .Returns(_exchangeProviderMock.Object);
+
+            _handler = new FindLatestCurrencyHandler(_factoryMock.Object,
                 new Mock<ILogger<FindLatestCurrencyHandler>>().Object);
         }
 
@@ -25,15 +30,26 @@ namespace UnitTests.Application
         public async Task GetProviderFromFactory()
         {
             await _handler.Handle(new FindLatestCurrencyQuery("GBP"), default);
-            _factory.Verify(x => x.GetProvider(ExchangeProviderType.Frankfurter), Times.Once);
+            _factoryMock.Verify(x => x.GetProvider(ExchangeProviderType.Frankfurter), Times.Once);
         }
 
         [Fact]
-        public async Task ReturnSystemErrorIfNoProvider()
+        public async Task FailIfNoProvider()
         {
+            _factoryMock.Setup(x => x.GetProvider(It.IsAny<ExchangeProviderType>()))
+              .Returns(() => null);
+
             var result = await _handler.Handle(new FindLatestCurrencyQuery("GBP"), default);
             result.IsFailure.Should().BeTrue();
             result.Error.Should().Be(Error.SystemError);
+        }
+
+        [Fact]
+        public async Task FailIfInvalidCurrency()
+        {
+            var result = await _handler.Handle(new FindLatestCurrencyQuery("INVALID"), default);
+            result.Error.Code.Should().Be(ErrorCode.BadInput);
+            result.Error.Message.Should().Be("The currency code is invalid");
         }
     }
 }
